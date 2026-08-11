24982a5611a5a58c33113d99de9f0de0e03efdd7
amani1116 <amanueldev03@gmail.com>
2026-08-11 23:35:38 +0300

    Add slug auto-generation and search/filtering to courses app

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -103,6 +103,11 @@ class Course(models.Model):
             models.Index(fields=["instructor"]),
         ]
 
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return self.title
 
